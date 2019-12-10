e8938664d553abf2cd412ff0509483b34aee0bc3
yoyo <jihu777@gmail.com>
2019-12-11 00:20:44 +0800

    remove

--- index.test.js
+++
@@ -1,8 +0,0 @@
-const pluginTester = require('babel-plugin-tester');
-const path = require('path');
-
-pluginTester.default({
-  pluginName: 'apollo-helper',
-  plugin: require('.'),
-  fixtures: path.join(__dirname, '__fixtures__')
-});
